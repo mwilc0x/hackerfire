ff8a021a00e936a1f90ee465def67bdef0ae71fd
Mike Wilcox <mwilcox56@gmail.com>
2014-10-12 13:53:47 -0400

    cleanup tests

--- test/index.js
+++ test/index.js
@@ -1,70 +1,68 @@
-'use strict';
+(function() {
+  'use strict';
 
-var chai = require('chai'),
-    should = chai.should(),
-    expect = chai.expect,
-    spies = require('chai-spies'),
-    hackerfire = require('hackerfire');
+  var chai = require('chai'),
+      should = chai.should(),
+      expect = chai.expect,
+      spies = require('chai-spies'),
+      hackerfire = require('hackerfire');
 
-describe('top stories', function() {
+  describe('hackerfire node module', function() {
 
-  beforeEach(function(){
-    chai.use(spies);
-  });
-
-  it('should return a list of top stories', function() {
-
-    hackerfire.getTopStories(function(res) {
-      expect(res).to.not.be.undefined;
-    });
-  });
-
-});
-
-describe('users', function() {
-
-  beforeEach(function(){
-    chai.use(spies);
-  });
-
-  it('should return a list of users with a single string input', function() {
-
-    hackerfire.getUserById('mot0rola', function(res) {
-      expect(res).to.not.be.undefined;
+    beforeEach(function(){
+      chai.use(spies);
     });
 
-  });
+    describe('top stories', function() {
 
-  it('should return a list of users with an array input of users', function() {
+      it('should return a list of top stories', function() {
 
-    hackerfire.getUserById(['pg', 'mot0rola'], function(res) {
-      expect(res).to.not.be.undefined;
+        hackerfire.getTopStories(function(res) {
+          expect(res).to.not.be.undefined;
+        });
+        
+      });
+
     });
 
-  });
+    describe('users', function() {
 
-});
+      it('should return a list of users with a single string input', function() {
 
-describe('item', function() {
+        hackerfire.getUserById('mot0rola', function(res) {
+          expect(res).to.not.be.undefined;
+        });
 
-  beforeEach(function(){
-    chai.use(spies);
-  });
+      });
 
-  it('should return a list of items with a single string input', function() {
+      it('should return a list of users with an array input of users', function() {
 
-    hackerfire.getUserById('8265435', function(res) {
-      expect(res).to.not.be.undefined;
+        hackerfire.getUserById(['pg', 'mot0rola'], function(res) {
+          expect(res).to.not.be.undefined;
+        });
+
+      });
+
     });
 
-  });
+    describe('item', function() {
 
-  it('should return a list of items with an array input of users', function() {
+      it('should return a list of items with a single string input', function() {
 
-    hackerfire.getUserById([ '8265435', '8168423' ], function(res) {
-      expect(res).to.not.be.undefined;
+        hackerfire.getUserById('8265435', function(res) {
+          expect(res).to.not.be.undefined;
+        });
+
+      });
+
+      it('should return a list of items with an array input of users', function() {
+
+        hackerfire.getUserById([ '8265435', '8168423' ], function(res) {
+          expect(res).to.not.be.undefined;
+        });
+
+      });
+
     });
-
   });
-
-});
+})();
